3f40cf104f23555d692cf33b9ca6873e3bb3f0dc
xoralife <abidhussainiouu@gmail.com>
2026-06-17 10:03:13 +0500

    add files

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
-from pro import products
+# from pro import products
 
 app = FastAPI()
 
+# @app.get("/")
+# def home():
+#     return {"message": "Hello World"}
 
-@app.get("/")
-def home():
-    return {"message": "Hello World"}
-
-
-
-@app.get("/contect")
-def home():
-    return products
+# @app.get("/contect")
+# def home():
+#     return products
